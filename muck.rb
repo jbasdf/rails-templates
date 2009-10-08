04d2369acd5eeb50b7183159630ff6a702ebb966
Justin Ball <justinball@gmail.com>
2009-10-07 18:52:27 -0600

    added fix for contact us within default controller

--- muck.rb
+++ muck.rb
@@ -412,7 +412,11 @@ file 'app/controllers/default_controller.rb',
   def contact
     return unless request.post?
     body = []
-    params.each_pair { |k,v| body << "#{k}: #{v}"  }
+    params.each_pair do |k,v|
+      if !['authenticity_token', 'action', 'controller'].include?(k) 
+        body << "#{k}: #{v}"
+      end
+    end
     BasicMailer.deliver_mail(:subject => I18n.t("contact.contact_response_subject", :application_name => GlobalConfig.application_name), :body=>body.join("\n"))
     flash[:notice] = I18n.t('general.thank_you_contact')
     redirect_to contact_url    
